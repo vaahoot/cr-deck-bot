421d24fe7932d095d4419ebbc09f04acd654ed0d
Volodymyr Vakhotskyi <vaahoot@gmail.com>
2026-06-11 23:47:47 +0100

    better logging functions

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,4 @@
+import datetime
 import unicodedata
 
 from playwright.async_api import Browser, Playwright
@@ -12,3 +13,19 @@ async def init_browser(playwright: Playwright) -> Browser:
 
 def normalise(text: str) -> str:
     return unicodedata.normalize("NFKC", text).lower().strip()
+
+
+def print_info(msg):
+    time = datetime.datetime.now()
+    formatted = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"{formatted} [INFO]", end="\t")
+    print(msg)
+
+
+def print_error(msg):
+    time = datetime.datetime.now()
+    formatted = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"{formatted} [ERROR]", end="\t")
+    print(msg)
